3b185e7bf466027b9329ea009a09c15717a499cc
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-31 12:58:32 +0300

    layout created

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,24 +3,7 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
     return ( 
         <>
-            <header>
-            <nav className="navbar navbar-light bg-primary">
-                <div className="container">
-                    <Link to="/" className="navbar-brand" href="#">DevPOS</Link>
-                </div>
-            </nav>
-            </header>
-
-            <main>
-                <div className="container mt-3">
-                    <div className="bg-light p-5 mt-4 rounded-3">
-                        <h1>Welcome to the simple POS for small business</h1>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                        <p>If you have an issue call 443-444-xxx anytime.</p>
-                        <Link to="/pospage">Click here to sell products</Link>
-                    </div>
-                </div>
-            </main>
+            
         </> 
     );
 }
