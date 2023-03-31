0018462bf2e0489a3a718274d81d5157a03b54e1
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-31 12:31:06 +0300

    Navbar / main content added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
     return ( 
         <>
-            <h1>HomePage</h1>
+            <header>
+            <nav className="navbar navbar-light bg-primary">
+                <div className="container">
+                    <Link to="/" className="navbar-brand" href="#">DevPOS</Link>
+                </div>
+            </nav>
+            </header>
+
+            <main>
+                <div className="container mt-3">
+                    <div className="bg-light p-5 mt-4 rounded-3">
+                        <h1>Welcome to the simple POS for small business</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                        <p>If you have an issue call 443-444-xxx anytime.</p>
+                        <Link to="/pospage">Click here to sell products</Link>
+                    </div>
+                </div>
+            </main>
         </> 
     );
 }
