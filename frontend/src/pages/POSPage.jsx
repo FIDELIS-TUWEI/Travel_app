112c092fa4b6e3d685e6bbe3d4170fd567bdc51d
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-31 13:32:39 +0300

    useEffect added / proxy.json

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import MainLayout from "../layouts/MainLayout";
 
 const POSPage = () => {
+    // fetching json data with useEffect hook
+    useEffect(() => {
+
+    }, []);
+
     return ( 
         <MainLayout>
             POS content
