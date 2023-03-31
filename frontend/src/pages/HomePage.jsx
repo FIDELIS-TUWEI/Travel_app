cbed46c66f613f12d2d194abd3438a30b9718703
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-31 13:12:07 +0300

    home fixed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout';
 
 const HomePage = () => {
     return ( 
-        <>
-            
-        </> 
+        <MainLayout>
+            <div className="bg-light pt-5 mt-4 rounded-3">
+                <h1>Welcome to the simple POS for small business</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, quo!</p>
+                <p>If you have an issue, call 443-444-xxx anytime</p>
+                <Link to="/pos" className='btn btn-primary'>Click here to sell products</Link>
+            </div>
+        </MainLayout> 
     );
 }
  
